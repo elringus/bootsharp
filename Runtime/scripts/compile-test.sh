2f6ec1665c3f193acf5cc34a570c25155659bd56
Artyom Sovetnikov <elringus@gmail.com>
2021-11-15 17:37:43 +0300

    kek

--- Runtime/scripts/compile-test.sh
+++ Runtime/scripts/compile-test.sh
@@ -2,5 +2,5 @@
 cd ../test/project
 rm -rf bin
 rm -rf obj
-dotnet publish -c Release #-fl -flp:logfile=Build.log;verbosity=diagnostic
+dotnet publish -c Release -fl -flp:logfile=Build.log;verbosity=diagnostic
 read -r -p "Press Enter key to exit..."
