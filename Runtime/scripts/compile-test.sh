80d5ccc8552ec6f07a5b04412e9a5f271abd75c5
Artyom Sovetnikov <elringus@gmail.com>
2021-11-15 15:22:09 +0300

    tt

--- Runtime/scripts/compile-test.sh
+++ Runtime/scripts/compile-test.sh
@@ -1,4 +1,4 @@
 # dotnet workload install wasm-tools --source https://api.nuget.org/v3/index.json
 cd ../test/project
-dotnet publish -c Release
+dotnet publish -c Release #-fl -flp:logfile=Build.log;verbosity=diagnostic
 read -r -p "Press Enter key to exit..."
