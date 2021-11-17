69c867091ac246f08eba24f3e529fb397c5a6bf2
Artyom Sovetnikov <elringus@gmail.com>
2021-11-18 01:55:17 +0300

    bench

--- Runtime/test/bench.js
+++ Runtime/test/bench.js
@@ -1,15 +1,17 @@
 ﻿const { boot, invoke, terminate } = require("./project/bin/dotnet");
+const assert = require("assert");
 
 describe("benchmark", () => {
     after(terminate);
     it("boot", boot);
-    it("compute", () => invoke("ComputePrime", 50000));
+    it("compute", () => invoke("ComputePrime", 52000));
     it("interop", () => {
-        for (let i = 0; i < 5000; i++) {
-            const instance = invoke("CreateInstance");
-            instance.invokeMethod("SetVar", "foo");
-            instance.invokeMethod("GetVar", "foo");
-            instance.dispose();
-        }
+        const cycles = 23000;
+        const instance = invoke("CreateInstance");
+        for (let i = 0; i <= cycles; i++)
+            instance.invokeMethod("SetVar", i.toString());
+        const performed = instance.invokeMethod("GetVar");
+        assert.deepStrictEqual(performed, cycles.toString());
+        instance.dispose();
     });
 });
