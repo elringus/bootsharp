b3118bffd3e1efa8acf843e0a261ec5c796e2396
Artyom Sovetnikov <elringus@gmail.com>
2021-11-27 01:24:19 +0300

    etc

--- DotNet/DotNetJS/JSFunctionAttribute.cs
+++ DotNet/DotNetJS/JSFunctionAttribute.cs
@@ -5,7 +5,7 @@ namespace DotNetJS
     /// <summary>
     /// Applied to a partial method to associate it with JavaScript function.
     /// The implementing JavaScript function is expected to be assigned
-    /// to the imported library object as 'lib.ClassName = { MethodName: function }'.
+    /// to the imported library object as 'lib.AssemblyName = { MethodName: function }'.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
     public sealed class JSFunctionAttribute : Attribute { }
