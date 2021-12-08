4cc15385c685648b4dcd414ae846eb977971cb92
Artyom Sovetnikov <elringus@gmail.com>
2021-12-08 20:56:28 +0300

    use file-scoped namespace

--- DotNet/DotNetJS/JS.cs
+++ DotNet/DotNetJS/JS.cs
@@ -3,51 +3,50 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.JSInterop;
 
-namespace DotNetJS
+namespace DotNetJS;
+
+/// <summary>
+/// Provides interop with JavaScript.
+/// </summary>
+public static class JS
 {
+    private static readonly JSRuntime js = new();
+
     /// <summary>
-    /// Provides interop with JavaScript.
+    /// Overrides default JSON serializer options used for marshalling the interop data.
     /// </summary>
-    public static class JS
-    {
-        private static readonly JSRuntime js = new();
+    public static void ConfigureJson (Action<JsonSerializerOptions> action) => js.ConfigureJson(action);
 
-        /// <summary>
-        /// Overrides default JSON serializer options used for marshalling the interop data.
-        /// </summary>
-        public static void ConfigureJson (Action<JsonSerializerOptions> action) => js.ConfigureJson(action);
+    /// <summary>
+    /// Invokes a global JavaScript function with the provided name and arguments.
+    /// </summary>
+    /// <param name="name">Name of the function to invoke.</param>
+    /// <param name="args">JSON-serializable arguments for the function.</param>
+    public static void Invoke (string name, params object[] args) => js.InvokeVoid(name, args);
 
-        /// <summary>
-        /// Invokes a global JavaScript function with the provided name and arguments.
-        /// </summary>
-        /// <param name="name">Name of the function to invoke.</param>
-        /// <param name="args">JSON-serializable arguments for the function.</param>
-        public static void Invoke (string name, params object[] args) => js.InvokeVoid(name, args);
+    /// <summary>
+    /// Invokes a global JavaScript function with the provided name and arguments.
+    /// </summary>
+    /// <param name="name">Name of the function to invoke.</param>
+    /// <param name="args">JSON-serializable arguments for the function.</param>
+    /// <typeparam name="T">Expected return type of the function.</typeparam>
+    /// <returns>The result of the function invocation.</returns>
+    public static T Invoke<T> (string name, params object[] args) => js.Invoke<T>(name, args);
 
-        /// <summary>
-        /// Invokes a global JavaScript function with the provided name and arguments.
-        /// </summary>
-        /// <param name="name">Name of the function to invoke.</param>
-        /// <param name="args">JSON-serializable arguments for the function.</param>
-        /// <typeparam name="T">Expected return type of the function.</typeparam>
-        /// <returns>The result of the function invocation.</returns>
-        public static T Invoke<T> (string name, params object[] args) => js.Invoke<T>(name, args);
+    /// <summary>
+    /// Invokes a global asynchronous JavaScript function with the provided name and arguments.
+    /// </summary>
+    /// <param name="name">Name of the function to invoke.</param>
+    /// <param name="args">JSON-serializable arguments for the function.</param>
+    /// <returns>A task that resolves when the asynchronous function returns.</returns>
+    public static ValueTask InvokeAsync (string name, params object[] args) => js.InvokeVoidAsync(name, args);
 
-        /// <summary>
-        /// Invokes a global asynchronous JavaScript function with the provided name and arguments.
-        /// </summary>
-        /// <param name="name">Name of the function to invoke.</param>
-        /// <param name="args">JSON-serializable arguments for the function.</param>
-        /// <returns>A task that resolves when the asynchronous function returns.</returns>
-        public static ValueTask InvokeAsync (string name, params object[] args) => js.InvokeVoidAsync(name, args);
-
-        /// <summary>
-        /// Invokes a global asynchronous JavaScript function with the provided name and arguments.
-        /// </summary>
-        /// <param name="name">Name of the function to invoke.</param>
-        /// <param name="args">JSON-serializable arguments for the function.</param>
-        /// <typeparam name="T">Expected return type of the function.</typeparam>
-        /// <returns>A task with the result that resolves when the asynchronous function returns.</returns>
-        public static ValueTask<T> InvokeAsync<T> (string name, params object[] args) => js.InvokeAsync<T>(name, args);
-    }
+    /// <summary>
+    /// Invokes a global asynchronous JavaScript function with the provided name and arguments.
+    /// </summary>
+    /// <param name="name">Name of the function to invoke.</param>
+    /// <param name="args">JSON-serializable arguments for the function.</param>
+    /// <typeparam name="T">Expected return type of the function.</typeparam>
+    /// <returns>A task with the result that resolves when the asynchronous function returns.</returns>
+    public static ValueTask<T> InvokeAsync<T> (string name, params object[] args) => js.InvokeAsync<T>(name, args);
 }
--- DotNet/DotNetJS/JSFunctionAttribute.cs
+++ DotNet/DotNetJS/JSFunctionAttribute.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 
-namespace DotNetJS
-{
-    /// <summary>
-    /// Applied to a partial method to bind it with a JavaScript function.
-    /// The function implementation is expected to be assigned as
-    /// 'dotnet.Assembly.Method = function' before the library is booted.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Method)]
-    public sealed class JSFunctionAttribute : Attribute { }
-}
+namespace DotNetJS;
+
+/// <summary>
+/// Applied to a partial method to bind it with a JavaScript function.
+/// The function implementation is expected to be assigned as
+/// 'dotnet.Assembly.Method = function' before the library is booted.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class JSFunctionAttribute : Attribute { }
