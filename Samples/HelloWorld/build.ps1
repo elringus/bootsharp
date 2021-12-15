d3cb4a5254cfea1f3174f22aca2d9338531d33eb
The Sharp Ninja <ninja@thesharp.ninja>
2021-12-15 09:21:34 -0600

    Added automation scripts.

---
+++ Samples/HelloWorld/build.ps1
@@ -0,0 +1,56 @@
+param (
+    [switch]
+    $Automated = $false
+)
+
+Push-Location
+
+Set-Location .\Project
+try {
+    & dotnet publish
+
+    Pop-Location
+    $popped = $true
+
+    $page = Get-Item .\global.html
+
+    if (-not $Automated) {
+        & MicrosoftEdge.exe $page.FullName
+
+        $title = 'Confirmation'
+        $question = 'Was publishing successful?'
+
+        $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+        $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'))
+        $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+
+        $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+    }
+    else {
+        $decision = 0;
+    }
+
+    if ($decision -eq 0) {
+        & node common.js
+
+        if($LASTEXITCODE -ne 0) {
+            throw "`$LASTEXITCODE from common.js is $LASTEXITCODE.";
+        }
+
+        & node es.mjs
+
+        if($LASTEXITCODE -ne 0) {
+            throw "`$LASTEXITCODE from es.mjs is $LASTEXITCODE.";
+        }
+    }
+    else {
+        Write-Host 'cancelled'
+    }
+
+    $LASTEXITCODE = 0;
+}
+finally {
+    if (!$popped) {
+        Pop-Location
+    }
+}
---
+++ build-all.ps1
@@ -0,0 +1,107 @@
+param (
+    [switch]
+    $Automated = $false,
+    [switch]
+    $Verbose = $false,
+    [switch]
+    $Clean = $false,
+    [switch]
+    $NoBuild = $false,
+    [switch]
+    $Publish = $false
+)
+
+Push-Location -Verbose:$Verbose
+$root = Get-Location -Verbose:$Verbose
+
+try {
+    if ($Clean) {
+        Get-ChildItem obj, bin, dist, node_modules -Recurse -ErrorAction SilentlyContinue -Verbose:$Verbose `
+        | Remove-Item -Force -Recurse -ErrorAction Stop -Verbose:$Verbose
+    }
+
+    if (-not $NoBuild) {
+        Set-Location $root/Dotnet -Verbose:$Verbose
+        $sln = Get-Item ./DotNetJS.sln -Verbose:$Verbose -ErrorAction SilentlyContinue
+
+        $continue = (Test-Path $sln -ErrorAction SilentlyContinue -Verbose:$Verbose) ?? $false
+
+        # Build DotNetJS Solution
+        if ($continue) {
+            $verbosity = $Verbose ? 'd' : 'n';
+            & dotnet build $sln.FullName -v $verbosity
+
+            $continue = $LASTEXITCODE -eq 0
+        }
+
+        # Build JavaScript JS Interop
+        if ($continue) {
+            $path = Resolve-Path $root/JavaScript/dotnet-js-interop -Verbose:$Verbose
+            if (Test-Path $path -Verbose:$Verbose) {
+                Set-Location $path -Verbose:$Verbose
+
+                & npm install
+                & npm run build
+
+                $continue = $LASTEXITCODE -eq 0
+            }
+            else {
+                throw "Could not locate JavaScript path at $path.";
+            }
+        }
+
+        # Build JavaScript dotnet-runtime
+        if ($continue) {
+            $path = Resolve-Path $root/JavaScript/dotnet-runtime -Verbose:$Verbose
+            if (Test-Path $path -Verbose:$Verbose) {
+                Set-Location $path -Verbose:$Verbose
+
+                & npm install
+                & npm run build
+
+                $continue = $LASTEXITCODE -eq 0
+            }
+            else {
+                throw "Could not locate JavaScript path at $path.";
+            }
+        }
+
+        # Build Samples
+        if ($continue) {
+            $path = Resolve-Path $root/Samples/HelloWorld -Verbose:$Verbose
+            if (Test-Path $path -Verbose:$Verbose) {
+                Set-Location $path -Verbose:$Verbose
+
+                . ./build.ps1 -Automated:$Automated -Verbose:$Verbose
+
+                $continue = $LASTEXITCODE -eq 0
+            }
+            else {
+                throw "Could not locate Sample path at $path.";
+            }
+        }
+
+        # Build Extension
+        if ($continue) {
+            $path = Resolve-Path $root/Samples/WebExtension -Verbose:$Verbose
+            if (Test-Path $path -Verbose:$Verbose) {
+                Set-Location $path -Verbose:$Verbose
+
+                & npm install
+                & npm run build
+
+                $continue = $LASTEXITCODE -eq 0
+            }
+            else {
+                throw "Could not locate Sample path at $path.";
+            }
+        }
+    }
+
+    if($Publish) {
+        # Steps to publish the components go here.
+    }
+}
+finally {
+    Pop-Location -Verbose:$Verbose
+}
