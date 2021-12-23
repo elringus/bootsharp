d56f88df1bfba58e1c41c91e3302a49bf7870f1a
Artyom Sovetnikov <elringus@gmail.com>
2021-12-23 04:57:08 +0300

    fix test

--- JavaScript/dotnet-runtime/test/csharp/Test.Main/Program.cs
+++ JavaScript/dotnet-runtime/test/csharp/Test.Main/Program.cs
@@ -1,6 +1,8 @@
-﻿using System.Text.Json.Serialization;
+﻿using System;
+using System.Text.Json.Serialization;
 using DotNetJS;
 using Microsoft.JSInterop;
+using Test.Types;
 
 namespace Test.Main;
 
@@ -15,11 +17,16 @@ public static class Program
         );
     }
 
-    private static void Main ()
+    public static void Main ()
     {
         mainInvoked = true;
+        ForceLoadTypesAssembly();
     }
 
     [JSInvokable]
     public static bool IsMainInvoked () => mainInvoked;
+
+    // Required to load Test.Types; otherwise JS throws 'assembly not loaded'. 
+    // TODO: Figure if that's by design and/or find a workaround.
+    private static void ForceLoadTypesAssembly () => Console.Write(typeof(Registry));
 }
