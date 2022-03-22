316c39015b7735cfa426ecae38ffd3e7157530f8
Artyom Sovetnikov <elringus@gmail.com>
2022-03-22 22:57:18 +0300

    emit enum js

--- DotNet/Packer/LibraryGenerator/LibraryGenerator.cs
+++ DotNet/Packer/LibraryGenerator/LibraryGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using static Packer.TextUtilities;
 
@@ -7,11 +8,17 @@ namespace Packer;
 internal class LibraryGenerator
 {
     private readonly HashSet<string> declaredObjects = new();
+    private readonly NamespaceBuilder spaceBuilder;
+
+    public LibraryGenerator (NamespaceBuilder spaceBuilder)
+    {
+        this.spaceBuilder = spaceBuilder;
+    }
 
     public string GenerateSideLoad (string runtimeJS, AssemblyInspector inspector)
     {
         return new LibraryTemplate {
-            InitJS = GenerateInitJS(inspector.Methods),
+            InitJS = GenerateInitJS(inspector),
             RuntimeJS = runtimeJS
         }.Build();
     }
@@ -25,18 +32,20 @@ internal class LibraryGenerator
             EntryAssemblyName = entryAssemblyName
         }.Build();
         return new LibraryTemplate {
-            InitJS = GenerateInitJS(inspector.Methods),
+            InitJS = GenerateInitJS(inspector),
             RuntimeJS = runtimeJS
         }.Build(embedJS);
     }
 
-    private string GenerateInitJS (IReadOnlyCollection<Method> methods)
+    private string GenerateInitJS (AssemblyInspector inspector)
     {
-        var invokable = methods.Where(m => m.Type == MethodType.Invokable);
-        var functions = methods.Where(m => m.Type == MethodType.Function);
+        var invokable = inspector.Methods.Where(m => m.Type == MethodType.Invokable);
+        var functions = inspector.Methods.Where(m => m.Type == MethodType.Function);
+        var enums = inspector.Types.Where(t => t.IsEnum);
         return JoinLines(
             JoinLines(invokable.Select(GenerateInvokableBinding)),
-            JoinLines(functions.Select(GenerateFunctionDeclaration))
+            JoinLines(functions.Select(GenerateFunctionDeclaration)),
+            JoinLines(enums.Select(GenerateEnumDeclaration))
         );
     }
 
@@ -54,6 +63,15 @@ internal class LibraryGenerator
     {
         var js = $"exports.{method.Namespace}.{method.Name} = undefined;";
         return EnsureNamespaceObjectsDeclared(method.Namespace, js);
+    }
+
+    private string GenerateEnumDeclaration (Type @enum)
+    {
+        var values = Enum.GetNames(@enum);
+        var fields = string.Join(", ", values.Select(v => $"{v}: \"{v}\""));
+        var space = spaceBuilder.Build(@enum);
+        var js = $"exports.{space}.{@enum.Name} = {{ {fields} }};";
+        return EnsureNamespaceObjectsDeclared(space, js);
     }
 
     private string EnsureNamespaceObjectsDeclared (string space, string js)
