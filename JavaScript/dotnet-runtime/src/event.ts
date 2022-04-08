8e430f281a28b2cf2e7a7caa699a476907b0bb8b
Artyom Sovetnikov <elringus@gmail.com>
2022-04-08 15:35:59 +0300

    add js event

---
+++ JavaScript/dotnet-runtime/src/event.ts
@@ -0,0 +1,26 @@
+﻿export class Event<T extends any[]> implements EventBroadcaster<T>, EventSubscriber<T> {
+    private readonly handlers = new Array<(...args: [...T]) => void>();
+
+    public broadcast(...args: [...T]) {
+        for (const handler of this.handlers)
+            handler(...args);
+    }
+
+    public subscribe(handler: (...args: [...T]) => void) {
+        this.handlers.push(handler);
+    }
+
+    public unsubscribe(handler: (...args: [...T]) => void) {
+        const index = this.handlers.indexOf(handler);
+        if (index >= 0) this.handlers.splice(index, 1);
+    }
+}
+
+export interface EventBroadcaster<T extends any[]> {
+    broadcast: (...args: [...T]) => void;
+}
+
+export interface EventSubscriber<T extends any[]> {
+    subscribe: (handler: (...args: [...T]) => void) => void;
+    unsubscribe: (handler: (...args: [...T]) => void) => void;
+}
---
+++ JavaScript/dotnet-runtime/test/event.js
@@ -0,0 +1,46 @@
+﻿const assert = require("assert");
+const { Event } = require("../dist/dotnet");
+
+describe("event", () => {
+    it("can broadcast without subscribers", () => {
+        assert.doesNotThrow(() => new Event().broadcast());
+    });
+    it("doesn't mind un-subscribing non-existent handler", () => {
+        assert.doesNotThrow(() => new Event().unsubscribe(null));
+    });
+    it("invokes subscribed handlers in order", () => {
+        let result = "";
+        const evt = new Event();
+        evt.subscribe(() => result = "foo");
+        evt.subscribe(() => result = "bar");
+        evt.broadcast();
+        assert.deepStrictEqual(result, "bar");
+    });
+    it("doesn't invoke un-subscribed handler", () => {
+        let result = false;
+        const evt = new Event();
+        const handler = v => result = v;
+        evt.subscribe(handler);
+        evt.broadcast(true);
+        evt.unsubscribe(handler);
+        evt.broadcast(false);
+        assert.deepStrictEqual(result, true);
+    });
+    it("delivers broadcast argument to the handlers", () => {
+        let result = "";
+        const evt = new Event();
+        evt.subscribe(v => result = v);
+        evt.broadcast("foo");
+        assert.deepStrictEqual(result, "foo");
+    });
+    it("subscribes and un-subscribes same handlers independently", () => {
+        let result = 0;
+        const evt = new Event();
+        const incrementer = () => result++;
+        for (let i = 0; i < 10; i++)
+            evt.subscribe(incrementer);
+        evt.unsubscribe(incrementer);
+        evt.broadcast();
+        assert.deepStrictEqual(result, 9);
+    });
+});
