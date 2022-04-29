806ae7b6fd6b69c632570b7c21b9be1ab0449294
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2022-04-29 19:05:10 +0300

    update samples (#55)

--- Samples/HelloWorld/node-es.mjs
+++ Samples/HelloWorld/node-es.mjs
@@ -1,7 +1,7 @@
-// Example on consuming the library as CommonJS module.
-// Run 'node common.js' to test.
+// Example on consuming the library as ECMAScript module.
+// Run 'node node-es.mjs' with node v17 or later to test.
 
-const dotnet = require("./Project/bin/dotnet");
+import dotnet from "./Project/bin/dotnet.js";
 
 // Providing implementation for 'GetHostName' function declared in 'HelloWorld' C# assembly.
 dotnet.HelloWorld.GetHostName = () => "Node.js";
@@ -11,5 +11,5 @@ dotnet.HelloWorld.GetHostName = () => "Node.js";
     await dotnet.boot();
     // Invoking 'GetName()' C# method defined in 'HelloWorld' assembly.
     const guestName = dotnet.HelloWorld.GetName();
-    console.log(`Welcome, ${guestName}! Enjoy your CommonJS module space.`);
+    console.log(`Welcome, ${guestName}! Enjoy your ECMAScript module space.`);
 })();
--- Samples/HelloWorld/node.js
+++ Samples/HelloWorld/node.js
@@ -1,7 +1,7 @@
-// Example on consuming the library as ECMAScript module.
-// Run 'node es.mjs' with node v17 or later to test.
+// Example on consuming the library as CommonJS module.
+// Run 'node node.js' to test.
 
-import dotnet from "./Project/bin/dotnet.js";
+const dotnet = require("./Project/bin/dotnet");
 
 // Providing implementation for 'GetHostName' function declared in 'HelloWorld' C# assembly.
 dotnet.HelloWorld.GetHostName = () => "Node.js";
@@ -11,5 +11,5 @@ dotnet.HelloWorld.GetHostName = () => "Node.js";
     await dotnet.boot();
     // Invoking 'GetName()' C# method defined in 'HelloWorld' assembly.
     const guestName = dotnet.HelloWorld.GetName();
-    console.log(`Welcome, ${guestName}! Enjoy your ECMAScript module space.`);
+    console.log(`Welcome, ${guestName}! Enjoy your CommonJS module space.`);
 })();
