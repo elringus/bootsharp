408aec4d02a0c8b73fa2d5adbf4a09f921e3a517
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2022-05-24 19:12:29 +0300

    add spec

--- JavaScript/dotnet-runtime/test/csharp/Test.Main/Function.cs
+++ JavaScript/dotnet-runtime/test/csharp/Test.Main/Function.cs
@@ -7,14 +7,14 @@ namespace Test.Main;
 public static partial class Function
 {
     [JSInvokable]
-    public static string TestEchoFunction (string value) => EchoFunction(value);
+    public static string[] TestEchoFunction (string[] values) => EchoFunction(values);
 
     [JSFunction]
-    public static partial string EchoFunction (string value);
+    public static partial string[] EchoFunction (string[] values);
 
     [JSInvokable]
-    public static Task<string> TestAsyncEchoFunction (string value) => AsyncEchoFunction(value);
+    public static Task<string[]> TestAsyncEchoFunction (string[] values) => AsyncEchoFunction(values);
 
     [JSFunction]
-    public static partial Task<string> AsyncEchoFunction (string value);
+    public static partial Task<string[]> AsyncEchoFunction (string[] values);
 }
