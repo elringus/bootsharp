e13d906d18d65f27b37f638a9663ac69378068c5
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2022-11-24 15:46:50 +0300

    bump versions

--- Samples/React/scripts/build-backend.sh
+++ Samples/React/scripts/build-backend.sh
@@ -6,5 +6,3 @@ printf "\nCopying build artifacts to public...\n\n"
 rm -rf ../public/bin
 cp -a ./Backend.WASM/bin/managed/. ../public/bin/
 cp ./Backend.WASM/bin/dotnet.wasm ../public/bin/dotnet.wasm
-
-read -r -p "Build complete. Press Enter key to exit..."
