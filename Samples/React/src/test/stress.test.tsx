9e26718a6ca8bb5a013672d46d82d5408ad5f138
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-01-21 08:18:50 +0300

    update react sample

--- Samples/React/src/test/stress.test.tsx
+++ Samples/React/src/test/stress.test.tsx
@@ -4,37 +4,37 @@ import { Backend, Frontend } from "backend";
 import { Stress } from "stress";
 
 beforeEach(() => {
-    Backend.StartStress = jest.fn();
-    Backend.StopStress = jest.fn();
-    Backend.IsStressing = jest.fn();
+    Backend.startStress = jest.fn();
+    Backend.stopStress = jest.fn();
+    Backend.isStressing = jest.fn();
 });
 
 test("stress is not running initially", () => {
     render(<Stress power={0}/>);
-    expect(Backend.StartStress).not.toBeCalled();
+    expect(Backend.startStress).not.toBeCalled();
 });
 
 test("get stress power returns value specified in props", async () => {
     render(<Stress power={666}/>);
-    expect(Frontend.GetStressPower()).toEqual(666);
+    expect(Frontend.getStressPower()).toEqual(666);
 });
 
 test("stress iteration time is written to screen", async () => {
     render(<Stress power={0}/>);
-    await act(() => Frontend.OnStressComplete.broadcast(13));
+    await act(() => Frontend.onStressComplete.broadcast(13));
     expect(screen.getByText(/Stressed over 13ms/));
 });
 
 test("button click stops stress when stress is running", async () => {
-    Backend.IsStressing = () => true;
+    Backend.isStressing = () => true;
     render(<Stress power={0}/>);
     await userEvent.click(screen.getByRole("button"));
-    expect(Backend.StopStress).toBeCalled();
+    expect(Backend.stopStress).toBeCalled();
 });
 
 test("button click starts stress when stress is not running", async () => {
-    Backend.IsStressing = () => false;
+    Backend.isStressing = () => false;
     render(<Stress power={0}/>);
     await userEvent.click(screen.getByRole("button"));
-    expect(Backend.StartStress).toBeCalled();
+    expect(Backend.startStress).toBeCalled();
 });
