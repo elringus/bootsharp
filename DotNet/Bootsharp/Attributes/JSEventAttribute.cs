17033ad1e40ce310c282d8ebed4e5ddeec4a1ee8
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-08-21 17:59:02 +0300

    iteration

--- DotNet/Bootsharp/Attributes/JSEventAttribute.cs
+++ DotNet/Bootsharp/Attributes/JSEventAttribute.cs
@@ -6,5 +6,12 @@ namespace Bootsharp;
 /// Applied to a partial method to bind it with an event meant to be
 /// broadcast (invoked) in C# and subscribed (listened) to in JavaScript.
 /// </summary>
+/// <example>
+/// <code>
+/// [JSEvent]
+/// public static partial string OnSomethingHappened (string payload);
+/// exports.MyApp.onSomethingHappened.subscribe(payload => ...);
+/// </code>
+/// </example>
 [AttributeUsage(AttributeTargets.Method)]
 public sealed class JSEventAttribute : Attribute { }
