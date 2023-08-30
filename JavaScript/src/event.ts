d91a78f1e99c0ad4aa699b207cae2de91a7a0db7
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-08-30 20:44:39 +0300

    iteration

---
+++ JavaScript/src/event.ts
@@ -0,0 +1,66 @@
+﻿export class Event<T extends unknown[]> implements EventBroadcaster<T>, EventSubscriber<T> {
+    private readonly handlers = new Map<string, (...args: [...T]) => void>();
+    private readonly warn: ((message: string) => void) | null;
+    private lastArgs: T | undefined;
+
+    constructor(warn?: ((message: string) => void) | null) {
+        this.warn = warn === undefined ? console.warn : warn;
+    }
+
+    public broadcast(...args: [...T]) {
+        this.lastArgs = args;
+        for (const handler of this.handlers.values())
+            handler(...args);
+    }
+
+    public subscribe(handler: (...args: [...T]) => void): string {
+        const id = this.getOrDefineId(handler);
+        this.subscribeById(id, handler);
+        return id;
+    }
+
+    public unsubscribe(handler: (...args: [...T]) => void) {
+        if (handler == null) return;
+        const id = this.getOrDefineId(handler);
+        this.unsubscribeById(id);
+    }
+
+    public subscribeById(id: string, handler: (...args: [...T]) => void): void {
+        if (this.handlers.has(id))
+            this.warn?.(`Failed to subscribe event handler with ID '${id}': handler is already subscribed.`);
+        else this.handlers.set(id, handler);
+    }
+
+    public unsubscribeById(id: string) {
+        if (this.handlers.has(id))
+            this.handlers.delete(id);
+        else this.warn?.(`Failed to unsubscribe event handler with ID '${id}': handler is not subscribed.`);
+    }
+
+    public getLast() {
+        return this.lastArgs;
+    }
+
+    private getOrDefineId(handler: (...args: [...T]) => void): string {
+        const prop = "bootsharpEventHandlerId";
+        if (handler.hasOwnProperty(prop))
+            return (handler as unknown as { [index: string]: string })[prop];
+        const id = crypto.randomUUID();
+        Object.defineProperty(handler, prop, {
+            value: id,
+            enumerable: false,
+            writable: false
+        });
+        return id;
+    }
+}
+
+export interface EventBroadcaster<T extends unknown[]> {
+    broadcast: (...args: [...T]) => void;
+}
+
+export interface EventSubscriber<T extends unknown[]> {
+    subscribe: (handler: (...args: [...T]) => void) => string;
+    unsubscribe: (handler: (...args: [...T]) => void) => void;
+    getLast: () => T | undefined;
+}
--- JavaScript/src/imports.ts
+++ JavaScript/src/imports.ts
@@ -1,21 +1,58 @@
-﻿import type { RuntimeAPI } from "./dotnet.d.ts";
+﻿// @ts-ignore (resolved when building C# solution)
+import * as bindings from "./bootsharp-bindings";
+import type { RuntimeAPI } from "./dotnet.d.ts";
+import type { Event } from "./event";
+
+type Binding = Invokable | VoidInvokable | AsyncInvokable | AsyncVoidInvokable | Event<[unknown?]>;
+type Invokable = (...args: unknown[]) => unknown;
+type VoidInvokable = (...args: unknown[]) => void;
+type AsyncInvokable = (...args: unknown[]) => Promise<unknown>;
+type AsyncVoidInvokable = (...args: unknown[]) => Promise<void>;
+
+const cached = new Map<string, Binding>;
 
 export function bindImports(runtime: RuntimeAPI) {
-    runtime.setModuleImports("Bootsharp", {
-        invoke: (endpoint: string, args?: string[]): string => {
-            return "";
-        },
-        invokeVoid: (endpoint: string, args?: string[]): void => {
+    runtime.setModuleImports("Bootsharp", { invoke, invokeVoid, invokeAsync, invokeVoidAsync, broadcast });
+}
 
-        },
-        invokeAsync: async (endpoint: string, args?: string[]): Promise<string> => {
-            return "";
-        },
-        invokeVoidAsync: async (endpoint: string, args?: string[]): Promise<void> => {
+function invoke(endpoint: string, args?: string[]): string {
+    return serialize(get<Invokable>(endpoint)(...deserialize(args)));
+}
 
-        },
-        broadcast: (endpoint: string, args?: string[]): void => {
+function invokeVoid(endpoint: string, args?: string[]): void {
+    get<VoidInvokable>(endpoint)(...deserialize(args));
+}
 
-        }
-    });
+async function invokeAsync(endpoint: string, args?: string[]): Promise<string> {
+    return serialize(await get<AsyncInvokable>(endpoint)(...deserialize(args)));
+}
+
+async function invokeVoidAsync(endpoint: string, args?: string[]): Promise<void> {
+    return get<AsyncVoidInvokable>(endpoint)(...deserialize(args));
+}
+
+function broadcast(endpoint: string, args?: string[]): void {
+    get<Event<[unknown?]>>(endpoint).broadcast(...deserialize(args));
+}
+
+function get<T extends Binding>(endpoint: string): T {
+    return (cached.get(endpoint) ?? cache(endpoint)) as T;
+}
+
+function cache(endpoint: string): Binding {
+    const binding = endpoint.split(".").reduce((x, y) => x[y], bindings);
+    cached.set(endpoint, binding);
+    return binding;
+}
+
+function deserialize(args?: string[]): unknown[] | never {
+    if (args == null || args.length === 0) return null as never;
+    const deserialized = new Array<unknown>(args.length);
+    for (let i = 0; i < args.length; i++)
+        deserialized[i] = JSON.parse(args[i]);
+    return deserialized;
+}
+
+function serialize(obj: unknown): string {
+    return JSON.stringify(obj);
 }
