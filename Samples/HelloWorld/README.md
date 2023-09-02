baece75539c156c43e932ac0c904d557d93022c6
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-09-03 02:56:29 +0300

    iteration

--- Samples/HelloWorld/README.md
+++ Samples/HelloWorld/README.md
@@ -2,4 +2,4 @@ Minimal example on using Bootsharp in browser and node.
 
 - Run `dotnet publish` under "backend" folder to compile the C# project
 - Run an html server (eg, `npx serve`) and navigate to `/browser.html` to test browser scenario
-- Run `node ./node.mjs` to test node scenario (requires node v17 or later)
+- Run `node node.mjs` to test node scenario (requires node v17 or later)
--- Samples/HelloWorld/node.mjs
+++ Samples/HelloWorld/node.mjs
@@ -5,9 +5,8 @@ import bootsharp, { HelloWorld } from "./backend/bin/bootsharp/bootsharp.js";
 
 // Binding 'GetHostName' method declared in 'HelloWorld' C# assembly.
 HelloWorld.getHostName = () => "Node.js";
-console.log(HelloWorld.getName())
 // Initializing dotnet runtime and invoking entry point.
 await bootsharp.boot();
 // Invoking 'GetName()' C# method defined in 'HelloWorld' assembly.
-// const name = HelloWorld.getName();
-// console.log(`Hello ${name}!`);
+const name = HelloWorld.getName();
+console.log(`Hello ${name}!`);
