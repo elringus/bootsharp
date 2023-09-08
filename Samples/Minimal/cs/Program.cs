f345854d5a2947e3e65d43e43543482bad09566a
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-09-08 18:06:02 +0300

    etc

--- Samples/Minimal/cs/Program.cs
+++ Samples/Minimal/cs/Program.cs
@@ -1,18 +1,29 @@
 ﻿// Specifying JavaScript APIs to generate bindings for.
+
+using System.Diagnostics.CodeAnalysis;
+
 [assembly: Bootsharp.JSImport(typeof(IFrontend))]
 // Specifying C# APIs to generate bindings for.
 [assembly: Bootsharp.JSExport(typeof(IBackend))]
 
-// Using generated C# bindings to inject implementation (usually handled by DI).
-_ = new Backend.JSBackend(new SharpBackend());
-// Using generated JavaScript bindings to invoke 'Frontend.getName()' function.
-System.Console.WriteLine($"Hello {Frontend.JSFrontend.GetName()}, .NET here!");
 
-// -------------------------------------------------------
-// TODO: Checking serialization, remove later.
-System.Console.WriteLine(Frontend.JSFrontend.GetInfo().Foo);
 public record Info(string Foo);
 // -------------------------------------------------------
+
+public static class Program
+{
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(Info))]
+    public static void Main ()
+    {
+        // Using generated C# bindings to inject implementation (usually handled by DI).
+        _ = new Backend.JSBackend(new SharpBackend());
+        // Using generated JavaScript bindings to invoke 'Frontend.getName()' function.
+        System.Console.WriteLine($"Hello {Frontend.JSFrontend.GetName()}, .NET here!");
+        // -------------------------------------------------------
+        // TODO: Checking serialization, remove later.
+        System.Console.WriteLine(Frontend.JSFrontend.GetInfo().Foo);
+    }
+}
 
 // Improvised API of JavaScript frontend.
 public interface IFrontend { string GetName (); Info GetInfo(); }
