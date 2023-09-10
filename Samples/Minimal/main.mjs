a48562eae4d5f4a66ae44e6dafff57a7d3bc2fcc
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-09-11 02:21:42 +0300

    etc

--- Samples/Minimal/main.mjs
+++ Samples/Minimal/main.mjs
@@ -3,8 +3,10 @@ import bootsharp, { Backend, Frontend } from "./cs/bin/bootsharp/bootsharp.js";
 
 // Binding 'IFrontend.GetName' endpoint invoked in C#.
 Frontend.getName = () =>
+    typeof Bun === "object" ? `Bun ${Bun.version}` :
     typeof Deno === "object" ? `Deno ${Deno.version.deno}` :
-    typeof Bun === "object" ? `Bun ${Bun.version}` : `Node ${process.version}`;
+    typeof process === "object" ? `Node ${process.version}` :
+    "Unknown JavaScript Runtime";
 // Initializing dotnet runtime and invoking entry point.
 await bootsharp.boot();
 // Invoking 'IBackend.GetName' C# method.
