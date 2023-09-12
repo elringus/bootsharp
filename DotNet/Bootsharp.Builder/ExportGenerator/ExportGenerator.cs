bb31b2b6154bdb3c214c987b088bcc30ba64ad8a
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-09-13 01:08:37 +0300

    etc

--- DotNet/Bootsharp.Builder/ExportGenerator/ExportGenerator.cs
+++ DotNet/Bootsharp.Builder/ExportGenerator/ExportGenerator.cs
@@ -2,7 +2,7 @@ namespace Bootsharp.Builder;
 
 internal sealed class ExportGenerator
 {
-    public static string Generate (AssemblyInspector inspector)
+    public string Generate (AssemblyInspector inspector)
     {
         return new ExportTemplate().Build();
     }
