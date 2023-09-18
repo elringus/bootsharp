8cc92b14757ee7523db6632aaf4c531a6d777a08
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-09-18 16:31:24 +0300

    brush deps

--- DotNet/Bootsharp.Generator.Test/Verifier.cs
+++ DotNet/Bootsharp.Generator.Test/Verifier.cs
@@ -5,9 +5,6 @@ using Microsoft.CodeAnalysis.Testing.Verifiers;
 
 namespace Bootsharp.Generator.Test;
 
-// The package is outdated on public nuget due to: https://github.com/dotnet/roslyn-sdk/issues/1047
-// .NET 7 and later sources: https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-tools/nuget/v3/index.json
-
 public sealed class Verifier<T> : CSharpSourceGeneratorTest<T, XUnitVerifier>
     where T : IIncrementalGenerator, new()
 {
