8ec35bb33868a5596e2acd4ec0ad1123684f0cca
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-09-20 19:52:47 +0300

    fix ci

--- JavaScript/scripts/compile-test.sh
+++ JavaScript/scripts/compile-test.sh
@@ -1,3 +1,5 @@
 # dotnet workload install wasm-tools --source https://api.nuget.org/v3/index.json
 # dotnet build-server shutdown
-dotnet publish ../tests/cs #-v:Normal -fl -flp:logfile=Build.log;verbosity=diagnostic
+cd ../tests/cs
+dotnet workload restore
+dotnet publish #-v:Normal -fl -flp:logfile=Build.log;verbosity=diagnostic
