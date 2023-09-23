d8576c3413f13a18a6771400a74221124cb466e8
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-09-23 22:07:41 +0300

    add ws test

--- JavaScript/tests/test/platform.mjs
+++ JavaScript/tests/test/platform.mjs
@@ -1,27 +1,27 @@
-// const assert = require("node:assert");
-// const { describe, it, before, after } = require("bun:test");
-// const dotnet = require("../dist/dotnet");
-// const { bootTest } = require("./cs");
-//
-// const invoke = (name, ...args) => dotnet.invoke("Test.Main", name, ...args);
-// const invokeAsync = (name, ...args) => dotnet.invokeAsync("Test.Main", name, ...args);
-//
-// describe("platform", () => {
-//     before(bootTest);
-//     after(dotnet.terminate);
-//     it("can provide guid", () => {
-//         const guid1 = invoke("GetGuid");
-//         const guid2 = invoke("GetGuid");
-//         assert.deepStrictEqual(guid1.length, 36);
-//         assert.deepStrictEqual(guid2.length, 36);
-//         assert.notDeepEqual(guid1, guid2);
-//     });
-//     it("can connect via websocket", async () => {
-//         global.WebSocket = require("ws");
-//         const wss = new WebSocket.Server({ port: 8080 });
-//         wss.on("connection", socket => socket.on("message", data => socket.send(data)));
-//         const echo = await invokeAsync("EchoViaWebSocket", "ws://localhost:8080", "foo", 1);
-//         assert.deepStrictEqual(echo, "foo");
-//         wss.close();
-//     });
-// });
+import assert from "node:assert";
+import { describe, it, before, after } from "node:test";
+import { boot, exit, Test } from "../cs.mjs";
+import ws, { WebSocketServer } from "ws";
+
+// .NET requires ws package when running on node:
+// https://github.com/dotnet/runtime/blob/main/src/mono/wasm/features.md#websocket
+global.WebSocket = ws;
+
+describe("platform", () => {
+    before(boot);
+    after(exit);
+    it("can provide unique guid", () => {
+        const guid1 = Test.getGuid();
+        const guid2 = Test.getGuid();
+        assert.strictEqual(guid1.length, 36);
+        assert.strictEqual(guid2.length, 36);
+        assert.notStrictEqual(guid1, guid2);
+    });
+    it("can connect via websocket", async () => {
+        const wss = new WebSocketServer({ port: 8080 });
+        wss.on("connection", socket => socket.on("message", data => socket.send(data)));
+        const echo = await Test.echoViaWebSocket("ws://localhost:8080", "foo", 1);
+        assert.deepStrictEqual(echo, "foo");
+        wss.close();
+    });
+});
