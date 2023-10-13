a277e39fad436f74308ae2eef64d33c3df04c4ae
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-10-13 03:36:51 +0300

    etc

--- JavaScript/test/spec/platform.spec.mjs
+++ JavaScript/test/spec/platform.spec.mjs
@@ -1,6 +1,6 @@
 import { describe, it, beforeAll, expect } from "vitest";
 import { bootSideload, Test } from "../cs.mjs";
-import ws, { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 
 describe("platform", () => {
     beforeAll(bootSideload);
@@ -14,7 +14,7 @@ describe("platform", () => {
     it("can communicate via websocket", async () => {
         // .NET requires ws package when running on node:
         // https://github.com/dotnet/runtime/blob/main/src/mono/wasm/features.md#websocket
-        global.WebSocket = ws;
+        global.WebSocket = WebSocket;
         const wss = new WebSocketServer({ port: 8080 });
         wss.on("connection", socket => socket.on("message", socket.send));
         expect(await Test.echoWebSocket("ws://localhost:8080", "foo", 3000)).toStrictEqual("foo");
