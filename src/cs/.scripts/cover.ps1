d783bf52813ed84071f27d5f5fe420bdfdf87510
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2024-01-04 20:38:23 +0300

    update cover script

--- src/cs/.scripts/cover.ps1
+++ src/cs/.scripts/cover.ps1
@@ -1,9 +1,13 @@
 try {
-	dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:ExcludeByAttribute=GeneratedCodeAttribute
-	reportgenerator "-reports:*/*.xml" "-targetdir:.cover" -reporttypes:HTML
-	serve .cover
-	python -m webbrowser http://localhost:3000
+    $out = "../.cover/"
+    $json = "../.cover/coverage.json"
+    dotnet test Bootsharp.Common.Test/Bootsharp.Common.Test.csproj /p:CollectCoverage=true /p:ExcludeByAttribute=GeneratedCodeAttribute /p:CoverletOutput=$out
+    dotnet test Bootsharp.Generate.Test/Bootsharp.Generate.Test.csproj /p:CollectCoverage=true /p:ExcludeByAttribute=GeneratedCodeAttribute /p:CoverletOutput=$out /p:MergeWith=$json
+    dotnet test Bootsharp.Inject.Test/Bootsharp.Inject.Test.csproj /p:CollectCoverage=true /p:ExcludeByAttribute=GeneratedCodeAttribute /p:CoverletOutput=$out /p:MergeWith=$json
+    dotnet test Bootsharp.Publish.Test/Bootsharp.Publish.Test.csproj /p:CollectCoverage=true /p:CoverletOutputFormat="json%2copencover" /p:ExcludeByAttribute=GeneratedCodeAttribute /p:CoverletOutput=$out /p:MergeWith=$json
+    reportgenerator "-reports:*/*.xml" "-targetdir:.cover" -reporttypes:HTML
+    python -m webbrowser http://localhost:3000
+    serve .cover
 } finally {
-	rm ./*/coverage.opencover.xml -force
-	rm ./.cover -r -force
+    rm .cover -r -force
 }
