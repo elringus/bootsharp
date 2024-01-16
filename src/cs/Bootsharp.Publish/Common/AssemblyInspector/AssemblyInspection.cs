632c0985bad6090f14610420ed6993ad331d58c5
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2024-01-17 02:23:55 +0300

    etc

--- src/cs/Bootsharp.Publish/Common/AssemblyInspector/AssemblyInspection.cs
+++ src/cs/Bootsharp.Publish/Common/AssemblyInspector/AssemblyInspection.cs
@@ -4,11 +4,11 @@ namespace Bootsharp.Publish;
 
 internal class AssemblyInspection (MetadataLoadContext ctx) : IDisposable
 {
-    public IReadOnlyCollection<AssemblyMeta> Assemblies { get; init; } = [];
-    public IReadOnlyCollection<InterfaceMeta> Interfaces { get; init; } = [];
-    public IReadOnlyCollection<MethodMeta> Methods { get; init; } = [];
-    public IReadOnlyCollection<Type> Crawled { get; init; } = [];
-    public IReadOnlyCollection<string> Warnings { get; init; } = [];
+    public required IReadOnlyCollection<AssemblyMeta> Assemblies { get; init; }
+    public required IReadOnlyCollection<InterfaceMeta> Interfaces { get; init; }
+    public required IReadOnlyCollection<MethodMeta> Methods { get; init; }
+    public required IReadOnlyCollection<Type> Crawled { get; init; }
+    public required IReadOnlyCollection<string> Warnings { get; init; }
 
     public void Dispose () => ctx.Dispose();
 }
