18d07b6379f3f47a258bf989728e42bcb3f2caca
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2024-01-30 11:55:56 +0300

    etc

--- src/cs/Bootsharp.Publish/Common/Preferences/Preferences.cs
+++ src/cs/Bootsharp.Publish/Common/Preferences/Preferences.cs
@@ -1,9 +1,14 @@
 ﻿namespace Bootsharp.Publish;
 
+/// <inheritdoc cref="JSPreferencesAttribute"/>
 internal sealed record Preferences
 {
+    /// <inheritdoc cref="JSPreferencesAttribute.Space"/>
     public IReadOnlyList<Preference> Space { get; init; } = [];
+    /// <inheritdoc cref="JSPreferencesAttribute.Type"/>
     public IReadOnlyList<Preference> Type { get; init; } = [];
+    /// <inheritdoc cref="JSPreferencesAttribute.Event"/>
     public IReadOnlyList<Preference> Event { get; init; } = [];
+    /// <inheritdoc cref="JSPreferencesAttribute.Function"/>
     public IReadOnlyList<Preference> Function { get; init; } = [];
 }
