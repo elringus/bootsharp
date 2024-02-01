437949ecf5a20c386106f5c4f47f6fd79162817e
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2024-02-01 10:59:25 +0300

    etc

--- src/js/src/instances.ts
+++ src/js/src/instances.ts
@@ -7,7 +7,7 @@ let nextId = Number.MIN_SAFE_INTEGER;
 
 /** Registers specified imported (JS -> C#) interop instance and associates it with unique ID.
  *  @param instance Interop instance to resolve ID for.
- *  @return Unique ID of the registered instance. */
+ *  @return Unique identifier of the registered instance. */
 export function registerInstance(instance: object): number {
     const id = idPool.length > 0 ? idPool.shift()! : nextId++;
     idToInstance.set(id, instance);
@@ -22,7 +22,7 @@ export function getInstance(id: number): object {
 
 /** Invoked from C# to notify that imported (JS -> C#) interop instance is no longer
  *  used (eg, was garbage collected) and can be released on JavaScript side as well.
- *  @param id ID of the disposed interop instance. */
+ *  @param id Unique identifier of the disposed interop instance. */
 export function disposeInstance(id: number): void {
     idToInstance.delete(id);
     idPool.push(id);
@@ -31,7 +31,7 @@ export function disposeInstance(id: number): void {
 /** Registers specified exported (C# -> JS) instance to invoke dispose on C# side
  *  when it's collected (finalized) by JavaScript runtime GC.
  *  @param instance Interop instance to register.
- *  @param id Unique ID of the interop instance. */
+ *  @param id Unique identifier of the interop instance. */
 export function disposeOnFinalize(instance: object, id: number): void {
     finalizer.register(instance, id);
 }
