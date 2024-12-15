8dfa509e4aa8c05156e35fbe2cf99deed1ddc8ed
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2024-12-15 18:28:39 +0300

    remove unused tests

--- src/cs/Bootsharp.Common.Test/InterfacesTest.cs
+++ src/cs/Bootsharp.Common.Test/InterfacesTest.cs
@@ -3,17 +3,9 @@
 public class InterfacesTest
 {
     [Fact]
-    public void Records ()
-    {
-        // TODO: Remove once coverlet properly handles record coverage.
-        _ = new ExportInterface(default, default) with { Interface = typeof(int) };
-        _ = new ImportInterface(default) with { Instance = "" };
-    }
-
-    [Fact]
     public void RegistersExports ()
     {
-        var export = new ExportInterface(typeof(IBackend), default);
+        var export = new ExportInterface(typeof(IBackend), null);
         Interfaces.Register(typeof(Backend), export);
         Assert.Equal(typeof(IBackend), Interfaces.Exports[typeof(Backend)].Interface);
     }
