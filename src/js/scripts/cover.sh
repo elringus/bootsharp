14c57eb2da095e95f65c9fec971ad4c80b5fc3f9
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2025-01-04 19:39:31 +0300

    update patcher

--- src/js/scripts/cover.sh
+++ src/js/scripts/cover.sh
@@ -1,3 +1,3 @@
-node --expose-gc ./node_modules/vitest/vitest.mjs run --silent \
+node ./node_modules/vitest/vitest.mjs run \
   --coverage.enabled --coverage.thresholds.100 --coverage.include=**/sideload/*.mjs \
   --coverage.exclude=**/dotnet.* --coverage.allowExternal
--- src/js/scripts/test.sh
+++ src/js/scripts/test.sh
@@ -1,1 +1,1 @@
-node --expose-gc ./node_modules/vitest/vitest.mjs run --silent
+node ./node_modules/vitest/vitest.mjs run
