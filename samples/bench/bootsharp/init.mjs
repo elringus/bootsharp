58cb62ee297932035191bff30a77f2174637b234
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2025-04-05 00:33:48 +0300

    sideload boot bins

--- samples/bench/bootsharp/init.mjs
+++ samples/bench/bootsharp/init.mjs
@@ -1,12 +1,21 @@
 import bootsharp, { Export, Import } from "./bin/bootsharp/index.mjs";
 import { getNumber, getStruct } from "../fixtures.mjs";
+import fs from "fs/promises";
 
 /** @returns {Promise<import("../bench.mjs").Exports>} */
 export async function init() {
     Import.getNumber = getNumber;
     Import.getStruct = getStruct;
 
-    await bootsharp.boot();
+    const content = await fs.readFile("./bootsharp/bin/bootsharp/bin/dotnet.native.wasm");
+    await bootsharp.boot({
+        root: "./bin",
+        resources: {
+            wasm: { name: "dotnet.native.wasm", content },
+            assemblies: [],
+            entryAssemblyName: "Boot.dll"
+        }
+    });
 
     return { ...Export };
 }
