2346e9917e2fc7276fcc23bf1aa7d052316f78e8
Artyom Sovetnikov <2056864+elringus@users.noreply.github.com>
2026-04-17 15:14:19 +0300

    feat!: use .net 10 runtime config (#194)

--- src/js/scripts/compile-test.sh
+++ src/js/scripts/compile-test.sh
@@ -1,4 +1,6 @@
 cd test/cs
+rm -rf Test/bin Test/obj Test.Types/bin Test.Types/obj
 dotnet restore --no-cache --force-evaluate
 dotnet publish -p BootsharpName=embedded -p BootsharpEmbedBinaries=true -c Debug
+rm -rf Test/bin/Debug Test/obj Test.Types/bin Test.Types/obj
 dotnet publish -p BootsharpName=sideload -p BootsharpEmbedBinaries=false -c Debug
