78138b0ddfe4797505155d51b165474d140515d4
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2026-05-20 12:06:05 +0300

    fix prefs

--- src/cs/Bootsharp.Publish/Common/Inspection/Meta/TypeMeta.cs
+++ src/cs/Bootsharp.Publish/Common/Inspection/Meta/TypeMeta.cs
@@ -21,9 +21,22 @@ internal record TypeMeta (Type Clr)
     /// <summary>
     /// The path to the module containing the node that represents the type in JavaScript.
     /// </summary>
-    public string JSModule { get; } = Slugify(WithPref(Pref.Space, Clr.Namespace ?? "index"));
+    public string JSModule { get; } = BuildModule(Clr);
     /// <summary>
     /// The path to the node inside the module that represents the type in JavaScript.
     /// </summary>
-    public string JSNode { get; } = WithPref(Pref.Name, TrimGeneric(Clr.FullName!).Split('.')[^1]).Replace('+', '.');
+    public string JSNode { get; } = BuildNode(Clr);
+
+    private static string BuildModule (Type Clr)
+    {
+        var slug = Slugify(WithPref(Pref.Space, Clr.Namespace ?? ""));
+        return string.IsNullOrWhiteSpace(slug) ? "index" : slug;
+    }
+
+    private static string BuildNode (Type Clr)
+    {
+        var full = TrimGeneric(Clr.FullName!);
+        var name = full[(full.LastIndexOf('.') + 1)..];
+        return WithPref(Pref.Name, name).Replace('+', '.');
+    }
 }
